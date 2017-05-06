a06ec7243a6d09973d2a91de4267804ab21c9743
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-06 14:21:44 -0400

    my_collect solution

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(collection)
+  if block_given?
+    i = 0
+    collection2 = []
+    while i < collection.length
+      yield collection[i]
+        if collection[i].include? " "
+            collection2 << collection[i].split(" ").first
+        else
+          collection2 << collection[i].upcase
+        end
+      i = i + 1
+    end
+    collection2
+  else
+     "No block given"
+  end
 
-
+end
